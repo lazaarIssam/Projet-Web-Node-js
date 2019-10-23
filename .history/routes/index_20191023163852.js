39c9667061654ec4110011aa0b24cb87056c6f35
li195592 <issam.lazaar@etu.univ-lehavre.fr>
2019-10-23 16:56:13 +0200

    'affichage/ajoute'

---
+++ .history/routes/index_20191023163852.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
+});
+
+/* GET Annoncelist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('annoncecollection');
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
+});
+
+module.exports = router;
---
+++ .history/routes/index_20191023165015.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
+/* GET Hello World page */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* Post addUser Function */
+router.post('/adduser', function(req, res) {
+  // Set DB variable
+  var db = req.db;
+  // Get our form values. These rely on the "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set our collection
+  var collection = db.get('usercollection');
+
+  // Submit to the DB
+  collection.insert({
+      "username" : userName,
+      "email" : userEmail
+  }, function (err, doc) {
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // And forward to success page
+          res.redirect("userlist");
+      }
+  });
+
+});
+
+/* GET Userlist page */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
+});
+
+/* GET Annoncelist page */
+router.get('/annoncelist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('annoncecollection');
+  collection.find({},{},function(e,docs){
+      res.render('annoncelist', {
+          "annoncelist" : docs
+      });
+  });
+});
+
+module.exports = router;
